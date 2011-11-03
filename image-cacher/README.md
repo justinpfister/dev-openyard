e5474e4e3e3368735f10e6d33dd7aab4d59c2711
justinpfister <justin@openyard.com>
2011-11-03 10:15:26 -0400

    working on image cacher

---
+++ image-cacher/README.md
@@ -0,0 +1,8 @@
+Openyard Image Cacher Sub-domain
+===============================
+
+- proxy the URI to Cloudfiles.
+-- if 404
+--- load image from server
+--- load image on cloudfiles
+- on re-load, image will load from cloudfiles
--- non-framework-www/test.php
+++ non-framework-www/test.php
@@ -1,5 +1,48 @@
 <?
-echo "php test";
-var_dump($_SERVER['REQUEST_URI']);
+
+//if (!isset($_GET['img'])) {
+//    echo 'no img';
+//    exit;
+//}
+
+    #echo "test works";
+    #exit;
+
+#echo $_GET['img'];
+
+// create curl resource
+        $ch = curl_init();
+
+        // set url
+        curl_setopt($ch, CURLOPT_URL, 'http://' . $_GET['img']);
+
+        //return the transfer as a string
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        // $output contains the output string
+        $output = curl_exec($ch);
+
+        // close curl resource to free up system resources
+        curl_close($ch);
+
+//Display the image in the browser
+//header('Content-type: image/jpg');
+
+
+if(($im = @imagecreatefromstring($output)) == FALSE) {
+    header("HTTP/1.0 404 Not Found");
+    exit;
+}
+  
+
+if ($im !== false) {
+    header('Content-Type: image/png');
+    imagepng($im);
+    imagedestroy($im);
+}
+else {
+    echo 'An error occurred.';
+}
+
 
 ?>
