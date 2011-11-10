bd01b0ec371dc650b074537e1cae74fe871f97b3
unknown <justin@frogtopT500.(none)>
2011-11-09 22:31:40 -0500

    removed silex-kitchen-edition link from app.php

--- openyard-src/app.php
+++ openyard-src/app.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/../Silex-KE/vendor/silex/autoload.php';
+require_once __DIR__ . '/../vendor/silex/autoload.php';
 
 use Silex\Provider\HttpCacheServiceProvider;
 use Silex\Provider\SessionServiceProvider;
