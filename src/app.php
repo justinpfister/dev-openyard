c3ea70532458d52b038c3420e75ea30d730bc34b
justinpfister <justin@openyard.com>
2011-11-10 16:57:59 -0500

    assetic at this point is wicked buggy.. for me at least.. no time to figure it out.. it's not a huge issue at this point.. maybe later.. for now.. just load required js files individually.

--- src/app.php
+++ src/app.php
@@ -82,45 +82,4 @@ $app->register(new DoctrineServiceProvider(), array(
 
 $app['session.storage'] = new DbalSessionStorage($app['db'], 'sessions');
 
-$app->register(new AsseticExtension(), array(
-    'assetic.options' => array(
-        'debug' => $app['debug']
-    ),
-    'assetic.filters' => $app->protect(function($fm) use ($app) {
-        $fm->set('yui_css', new Assetic\Filter\Yui\CssCompressorFilter(
-            $app['assetic.filter.yui_compressor.path']
-        ));
-        $fm->set('yui_js', new Assetic\Filter\Yui\JsCompressorFilter(
-            $app['assetic.filter.yui_compressor.path']
-        ));
-    }),
-    'assetic.assets' => $app->protect(function($am, $fm) use ($app) {
-        $am->set('styles', new Assetic\Asset\AssetCache(
-            new Assetic\Asset\GlobAsset(
-                $app['assetic.input.path_to_css'],
-                // Yui compressor is disabled by default.
-                // If you need it, and you have installed it, uncomment the
-                // next line, and delete "array()"
-                //array($fm->get('yui_css'))
-                array()
-            ),
-            new Assetic\Cache\FilesystemCache($app['assetic.path_to_cache'])
-        ));
-        $am->get('styles')->setTargetPath($app['assetic.output.path_to_css']);
-
-        $am->set('scripts', new Assetic\Asset\AssetCache(
-            new Assetic\Asset\GlobAsset(
-                $app['assetic.input.path_to_js'],
-                // Yui compressor is disabled by default.
-                // If you need it, and you have installed it, uncomment the
-                // next line, and delete "array()"
-                array($fm->get('yui_js'))
-                //array()
-            ),
-            new Assetic\Cache\FilesystemCache($app['assetic.path_to_cache'])
-        ));
-        $am->get('scripts')->setTargetPath($app['assetic.output.path_to_js']);
-    })
-));
-
 return $app;
