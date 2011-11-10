86fa0b19981d6b258791e4e95868c121cfc35896
justinpfister <justin@openyard.com>
2011-11-10 18:45:08 -0500

    added js and css back to resources

--- src/config.php
+++ src/config.php
@@ -28,3 +28,19 @@ $app['cache.path'] = __DIR__ . '/../cache';
 
 // Http cache
 $app['http_cache.cache_dir'] = $app['cache.path'] . '/http';
+
+// Assetic
+$app['assetic.path_to_cache']       = $app['cache.path'] . DIRECTORY_SEPARATOR . 'assetic' ;
+$app['assetic.path_to_web']         = __DIR__ . '/../web/assets';
+
+$app['assetic.input.path_to_assets']    = __DIR__ . '/../resources/assets';
+$app['assetic.input.path_to_css']       = $app['assetic.input.path_to_assets'] . '/css/*.css';
+$app['assetic.output.path_to_css']      = '/css/styles.css';
+$app['assetic.input.path_to_js']        = array(
+    $app['assetic.input.path_to_assets'] . '/js/bootstrap-twipsy.js',
+    $app['assetic.input.path_to_assets'] . '/js/bootstrap-*.js',
+    $app['assetic.input.path_to_assets'] . '/js/script.js',
+);
+$app['assetic.output.path_to_js']       = '/js/scripts.js';
+
+$app['assetic.filter.yui_compressor.path'] = '/usr/share/yui-compressor/yui-compressor.jar';
